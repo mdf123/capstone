fb36143737c4d90aa8a7a0d0f45286171c41a7dc
Dr. Hans M. Rupp <hansrupp@aol.com>
2022-07-17 09:10:43 +0200

    Comments formatting

--- projekct_local_data.R
+++ projekct_local_data.R
@@ -1,9 +1,12 @@
+#Capstone project for HarvardX PH125.9x Data Science
+#Hans M. Rupp 2022
+
 rm(list=ls())
 library(tidyverse)
 library(caret)
 library(gridExtra)
 
-
+#
 load(file="data/edx")
 load(file="data/validation")
 load(file="data/movies")
@@ -47,7 +50,7 @@ edx %>% group_by(movieId) %>% summarise(n=n()) %>% ggplot(aes(x=n)) + geom_histo
 #Average rating per movie
 edx %>% group_by(movieId) %>% summarise(movie_mean = mean(rating)) %>% ggplot(aes(x=movie_mean)) + geom_histogram() + xlab("Average rating per movie")
 
-edx %>% head(3)
+
 #Add release Year
 release_year_edx <- as.numeric(str_sub(edx$title, start=-5, end=-2))
 edx <- edx %>% mutate(year=release_year_edx)
@@ -57,50 +60,53 @@ head(validation)
 
 edx %>% group_by(year) %>% summarise(avg_rating = mean(rating)) %>% ggplot(aes(x=year, y=avg_rating)) + geom_point() + geom_smooth()
 
+#Define the loss function Root Mean Square Error
 RMSE <- function(true_ratings, predicted_ratings){
   sqrt(mean((true_ratings - predicted_ratings)^2))
 }
 
+#Naive model: only using the mean
 mu_rating <- mean(edx$rating)
 rmse_mean <- RMSE(validation$rating, mu_rating)
 rmse_mean
 
+#A tibble for keeping the RMSEs of the different models
 models <- tibble(model="Mean", rmse=rmse_mean)
 models
 
+## Effect of different movies
+#Calculate the average rating b_i for each movie minus the overall average rating mu_rating:
 movie_avgs <- edx %>% group_by(movieId) %>% summarise(b_i = mean(rating - mu_rating))
 
+#Plot them:
 movie_avgs %>% ggplot(aes(b_i)) + geom_histogram(bins = 10)
 
-
-
-head(edx)
+#Join the movie_avg data.frame to the edx (training) and validation set:
 
 edx_movies <- edx %>% left_join(movie_avgs, by="movieId")
-
-head(edx_movies)
-
 validation_movies <- validation %>% left_join(movie_avgs, by="movieId")
-head(validation_movies)
 
+#Calculating the predicted ratings
 predicted_ratings_movies <- mu_rating + validation_movies$b_i
 
 
 rmse_movies <- RMSE(predicted_ratings_movies, validation$rating)
 models <- rbind(models, c("movies", rmse_movies))
-models
+
 
 #user effects
 
+#Calculate the average rating b_u for each movie minus the overall average rating mu_rating and b_i:
 user_avg <- edx_movies %>% group_by(userId) %>% summarise(b_u = mean(rating - mu_rating - b_i)) 
-head(user_avg)
 
+#Join the user_avg data.frame to the edx_movies (training) and validation_movies set:
 edx_movies_users <- edx_movies %>% left_join(user_avg, by="userId")
 validation_movies_users <- validation_movies %>% left_join(user_avg, by="userId")
 
-
+#Plot the distribution of user effects b_u
 edx_movies_users %>% ggplot(aes(x=b_u)) + geom_histogram()
 
+#Calculating the predicted ratings
 predicted_ratings_movies_users <- mu_rating + validation_movies_users$b_i + validation_movies_users$b_u
 rmse_movies_users <- RMSE(predicted_ratings_movies_users, validation$rating)
 rmse_movies_users
@@ -108,15 +114,18 @@ rmse_movies_users
 models <- rbind(models, c("movies users", rmse_movies_users))
 models
 
-#year effect
-
+#Effect of the release year
+#Calculate the average rating b_u for each movie minus the overall average rating mu_rating and b_i and b_u:
 year_avg <- edx_movies_users %>% group_by(year) %>% summarise(b_y = mean(rating - mu_rating - b_i - b_u))
 
+#Plot the effect of the release year b_y
 year_avg %>% ggplot(aes(x=b_y)) + geom_histogram()
 
+#Join the year_avg data.frame to the edx_movies_users (training) and validation_movies_users set:
 edx_movies_users_years <- edx_movies_users %>% left_join(year_avg, by="year")
 validation_movies_users_years <- validation_movies_users %>% left_join(year_avg, by="year")
 
+#Calculating the predicted ratings
 predicted_ratings_movies_users_years <- mu_rating + validation_movies_users_years$b_i + validation_movies_users_years$b_u + validation_movies_users_years$b_y
 rmse_movies_users_years <- RMSE(predicted_ratings_movies_users_years, validation$rating)
 rmse_movies_users_years
@@ -124,15 +133,20 @@ rmse_movies_users_years
 models <- rbind(models, c("movies users years", rmse_movies_users_years))
 models
 
-#genre effect
 
-head(edx_movies_users_years)
+#Effect of the genre
+
+#Calculate the average rating b_g for each movie minus the overall average rating mu_rating and b_i and b_u and b_y
 genre_avg <- edx_movies_users_years %>% group_by(genres) %>% summarise(b_g = mean(rating - mu_rating - b_i - b_u - b_y))
+#Plot the effect of the genre b_g
 genre_avg %>% ggplot(aes(x=b_g)) + geom_histogram()
 
+
+#Join the genre_avg data.frame to the edx_movies_users_year (training) and validation_movies_users_year set
 edx_movies_users_years_genres <- edx_movies_users_years %>% left_join(genre_avg, by="genres")
 validation_movies_users_years_genres <- validation_movies_users_years %>% left_join(genre_avg, by="genres")
 
+#Calculating the predicted ratings
 predicted_ratings_movies_users_years_genres <- mu_rating + validation_movies_users_years_genres$b_i + validation_movies_users_years_genres$b_u  + validation_movies_users_years_genres$b_y +  + validation_movies_users_years_genres$b_g
 rmse_movies_users_years_genres <- RMSE(predicted_ratings_movies_users_years_genres, validation$rating)
 rmse_movies_users_years
@@ -142,6 +156,7 @@ models
 
 #Regularization
 
+#Optimization of lambda
 test_lambdas <- function(l) {
   movie_avgs_r <- edx %>% group_by(movieId) %>% summarise(b_i = sum(rating - mu_rating) /(n()+l))
   edx_movies_r <- edx %>% left_join(movie_avgs_r, by="movieId")
@@ -167,6 +182,7 @@ test_lambdas <- function(l) {
 lambdas <- seq(0,10, 1)
 rmses <- sapply(lambdas, test_lambdas)
 
+#Potting the RMSEs for different lambdas
 dfr <- data.frame(lambdas, rmses)
 dfr %>% ggplot(aes(x=lambdas, y=rmses)) + geom_point() + labs(x="Lambda", y="RMSE")
 
@@ -194,5 +210,6 @@ predictions <- mu_rating + validation_movies_users_years_genres_r$b_i + validati
 rmse_movies_users_years_genres_reg <- RMSE(predictions, validation$rating)
 rmse_movies_users_years_genres_reg
 
+#Final comparison
 models <- rbind(models, c("movies users years genres regular", rmse_movies_users_years_genres))
 models
